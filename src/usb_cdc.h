3a661129b0c5bfd66754d73c5f791deefa262cfe
Daniel Campoverde [alx741] <alx741@riseup.net>
2015-03-14 11:49:00 -0500

    Clean comments: usb_cdc.h

--- src/usb_cdc.h
+++ src/usb_cdc.h
@@ -3,9 +3,9 @@
  * Compiler: sdcc (Version 3.4.0)
  *
  *
- * [!] This file cotains USB definitions described by
- * 	   usb 2.0, [CDC] Communications device class specification 
- * 	   and [ACM/PSTN] device subclass specification at usb.org
+ * [!] This file contains USB definitions described by USB 2.0, [CDC]
+ * Communications device class specification and [ACM/PSTN] device subclass
+ * specification
  *
  *
  *
@@ -29,47 +29,43 @@
 
 
 
-/************************************************
- * **********************************************
- *
- * CDC BUFFERS
- * 64bytes RX, 64bytes TX
- *
- * **********************************************
- ************************************************/
+/*******************************************************************************
+                                  CDC BUFFERS
+
+                             64bytes RX, 64bytes TX
+*******************************************************************************/
 
 // COM BUFFERS SIZES
-#define USB_CDC_RX_BUFFER_SIZE 64 // Data interface bulk out endpoint
-#define USB_CDC_TX_BUFFER_SIZE 64 // Data interface bulk in endpoint
+#define USB_CDC_RX_BUFFER_SIZE 64 // Data interface bulk OUT endpoint
+#define USB_CDC_TX_BUFFER_SIZE 64 // Data interface bulk IN endpoint
 
 // CDC BUFFERS
-extern volatile unsigned char USB_CDC_RX_BUFFER[CDC_RX_BUFFER_SIZE];
-extern volatile unsigned char USB_CDC_TX_BUFFER[CDC_TX_BUFFER_SIZE];
+extern volatile unsigned char USB_CDC_RX_BUFFER;
+extern volatile unsigned char USB_CDC_TX_BUFFER;
+
+/*******************************************************************************
+*******************************************************************************/
 
 
 
+/*******************************************************************************
+                                   CDC DEVICE
 
-/************************************************
- * **********************************************
- *
- * CDC DEVICE
- * (usb 2.0, cdc specification, page 12, table 2)
- *
- * **********************************************
- ************************************************/
+                See USB 2.0: CDC specification page 12 table 2
+*******************************************************************************/
 
 #define USB_CDC_CLASS_DEVICE 0x02 // Communications device class
 
+/*******************************************************************************
+*******************************************************************************/
 
 
-/************************************************
- * **********************************************
- *
- * CDC/ACM REQUESTS
- * (usb 2.0, pstn specification, page 19, table 11)
- *
- * **********************************************
- ************************************************/
+
+/*******************************************************************************
+                                CDC/ACM REQUESTS
+
+                See USB 2.0: PSTN specification page 19 table 11
+*******************************************************************************/
 
 #define USB_CDC_REQUEST_SEND_ENCAPSULATED_COMMAND 0x00
 #define USB_CDC_REQUEST_GET_ENCAPSULATED_RESPONSE 0x01
@@ -81,95 +77,94 @@ extern volatile unsigned char USB_CDC_TX_BUFFER[CDC_TX_BUFFER_SIZE];
 #define USB_CDC_REQUEST_SET_CONTROL_LINE_STATE 0x22
 #define USB_CDC_REQUEST_SEND_BREAK 0x23
 
+/*******************************************************************************
+*******************************************************************************/
 
 
-/************************************************
- * **********************************************
- *
- * CDC/ACM NOTIFICATIONS
- * (usb 2.0, pstn specification, page 31, table 28)
- *
- * **********************************************
- ************************************************/
+
+/*******************************************************************************
+                             CDC/ACM NOTIFICATIONS
+
+                See USB 2.0: PSTN specification page 31 table 28
+*******************************************************************************/
 
 #define USB_CDC_NOTIFICATION_NETWORK_CONNECTION 0x00
 #define USB_CDC_NOTIFICATION_RESPONSE_AVAILABLE 0x01
 #define USB_CDC_NOTIFICATION_SERIAL_STATE 0x20
 
-
-
-/************************************************
- * **********************************************
- *
- * COMMUNICATIONS INTERFACE
- * (usb 2.0, cdc specification, page 12,
- * table 3,4,5)
- *
- * **********************************************
- ************************************************/
-
-// Field: bInterfaceClass
-#define USB_CDC_CLASS_INTERFACE_COM 0x02 // Communications interface class
-
-// Field: bInterfaceSubClass
-#define USB_CDC_SUBCLASS_INTERFACE_ACM 0x02 // Abstract control model interface subclass
-
-// Field: bInterfaceProtocol
-#define USB_CDC_PROTOCOL_INTERFACE_V250 0x01 // At commands: v250 interface protocol
+/*******************************************************************************
+*******************************************************************************/
 
 
 
-/************************************************
- * **********************************************
- *
- * DATA INTERFACE
- * (usb 2.0, cdc specification, page 13,
- * table 6)
- *
- * **********************************************
- ************************************************/
+/*******************************************************************************
+                            COMMUNICATIONS INTERFACE
+
+               See USB 2.0: CDC specification page 12 table 3,4,5
+*******************************************************************************/
 
 // Field: bInterfaceClass
-#define USB_CDC_CLASS_INTERFACE_DAT 0x0A // Data interface class
+#define USB_CDC_CLASS_INTERFACE_COM 0x02
 
 // Field: bInterfaceSubClass
-#define USB_CDC_SUBCLASS_INTERFACE_NONE 0x00 // No interface subclass
+#define USB_CDC_SUBCLASS_INTERFACE_ACM 0x02
 
 // Field: bInterfaceProtocol
-#define USB_CDC_PROTOCOL_INTERFACE_NONE 0x00 // No interface protocol
+#define USB_CDC_PROTOCOL_INTERFACE_V250 0x01
+
+/*******************************************************************************
+*******************************************************************************/
 
 
 
-/************************************************
- * **********************************************
- *
- * FUNCTIONAL DESCRIPTORS
- * (usb 2.0, cdc specification, page 16,
- * table 11, 12, 13, 14)
- *
- * (usb 2.0, pstn specification, page 16,
- * table 11, 12, 13, 14)
- *
- * **********************************************
- ************************************************/
+/*******************************************************************************
+                                 DATA INTERFACE
+
+                 See USB 2.0: CDC specification page 13 table 6
+*******************************************************************************/
+
+// Field: bInterfaceClass
+#define USB_CDC_CLASS_INTERFACE_DAT 0x0A
+
+// Field: bInterfaceSubClass
+#define USB_CDC_SUBCLASS_INTERFACE_NONE 0x00
+
+// Field: bInterfaceProtocol
+#define USB_CDC_PROTOCOL_INTERFACE_NONE 0x00
+
+/*******************************************************************************
+*******************************************************************************/
+
+
+
+/*******************************************************************************
+                             FUNCTIONAL DESCRIPTORS
+
+          See USB 2.0: CDC specification page 16 table 11, 12, 13, 14
+
+          See USB 2.0: PSTN specification page 16 table 11, 12, 13, 14
+*******************************************************************************/
 
 // FUNCTIONAL DESCRIPTOR TYPES
 // Field: bDescriptorType
-#define USB_CDC_FUNCTIONAL_CS_INTERFACE 0x24 // Interface functional descriptor
-#define USB_CDC_FUNCTIONAL_CS_ENDPOINT 0x25 // Endpoint functional descriptor
+#define USB_CDC_FUNCTIONAL_CS_INTERFACE 0x24
+#define USB_CDC_FUNCTIONAL_CS_ENDPOINT 0x25
 
 // FUNCTIONAL DESCRIPTOR SUBTYPES
 // Field: bDescriptorSubType
-#define USB_CDC_FUNCTIONAL_HEADER 0x00 // Header functional descriptor
-#define USB_CDC_FUNCTIONAL_CALL_MANAGEMENT 0x01 // Call management functional descriptor
-#define USB_CDC_FUNCTIONAL_ACM 0x02 // Abstract control management functional descriptor
-#define USB_CDC_FUNCTIONAL_UNION 0x06 // Union functional descriptor
+#define USB_CDC_FUNCTIONAL_HEADER 0x00
+#define USB_CDC_FUNCTIONAL_CALL_MANAGEMENT 0x01
+#define USB_CDC_FUNCTIONAL_ACM 0x02
+#define USB_CDC_FUNCTIONAL_UNION 0x06
 
 
-/* ---------------------------------------------
- * HEADER FUNCTIONAL DESCRIPTOR
- * (usb 2.0, cdc specification, page 18, table 15)
- * ---------------------------------------------*/
+
+/*
+ * -----------------------------------------------
+ *          HEADER FUNCTIONAL DESCRIPTOR
+ * See USB 2.0: CDC specification page 18 table 15
+ * -----------------------------------------------
+ */
 
 // Field: bcdCDC
 #define USB_CDC_HEADER_BCDUSB 0x0110 // USB CDC 1.2 compliant device
@@ -184,10 +179,13 @@ typedef struct
 } USB_CDC_DESCRIPTOR_FUNCTIONAL_HEADER_t;
 
 
-/* ---------------------------------------------
- * UNION FUNCTIONAL DESCRIPTOR
- * (usb 2.0, cdc specification, page 19, table 16)
- * ---------------------------------------------*/
+
+/*
+ * -----------------------------------------------
+ *          UNION FUNCTIONAL DESCRIPTOR
+ * See USB 2.0: CDC specification page 19 table 16
+ * -----------------------------------------------
+ */
 
 typedef struct
 {
@@ -199,13 +197,17 @@ typedef struct
 } USB_CDC_DESCRIPTOR_FUNCTIONAL_UNION_t;
 
 
-/* ---------------------------------------------
- * CALL MANAGEMENT FUNCTIONAL DESCRIPTOR
- * (usb 2.0, pstn specification, page 11, table 3)
- * ---------------------------------------------*/
+
+/*
+ * -----------------------------------------------
+ *    CALL MANAGEMENT FUNCTIONAL DESCRIPTOR
+ * See USB 2.0: PSTN specification page 11 table 3
+ * -----------------------------------------------
+ */
 
 // Field: bmCapabilities
-#define USB_CDC_CALL_MANAGEMENT_BMCAPABILITIES 0x00 // Device does not handle call management itself
+// Device does not handle call management itself
+#define USB_CDC_CALL_MANAGEMENT_BMCAPABILITIES 0x00
 
 
 typedef struct
@@ -218,10 +220,13 @@ typedef struct
 } USB_CDC_DESCRIPTOR_FUNCTIONAL_CALL_MANAGEMENT_t;
 
 
-/* ---------------------------------------------
+
+/*
+ * -------------------------------------------------
  * ABSTRACT CONTROL MANAGEMENT FUNCTIONAL DESCRIPTOR
- * (usb 2.0, pstn specification, page 12, table 4)
- * ---------------------------------------------*/
+ * See USB 2.0: PSTN specification page 12 table 4
+ * -------------------------------------------------
+ */
 
 // Field: bmCapabilities
 #define USB_CDC_ACM_BMCAPABILITIES 0x02 // Support "line" requests
@@ -234,5 +239,9 @@ typedef struct
 	unsigned char bDescriptorSubType;
 	unsigned char bmCapabilities;
 } USB_CDC_DESCRIPTOR_FUNCTIONAL_ABSTRACT_CONTROL_MANAGEMENT_t;
+
+/*******************************************************************************
+*******************************************************************************/
+
 
 #endif // _USB_CDC_H
