635f1f7d53dcc4f9b79616d85cb40687e40e3c40
Daniel Campoverde [alx741] <alx741@riseup.net>
2016-02-11 19:38:06 -0500

    Fix UART util code style

--- src/util/uart.c
+++ src/util/uart.c
@@ -5,49 +5,49 @@
 void uart_init(void)
 {
     SPBRGH = 0;
-	SPBRG = 77;
-	TXSTAbits.BRGH = 0;
-	BAUDCONbits.BRG16 = 0;
-	TXSTAbits.SYNC = 0;
-	RCSTAbits.SPEN = 1;
-	PIE1bits.TXIE = 0;
-	TXSTAbits.TX9 = 0;
-	TXSTAbits.TXEN = 1;
-	PIE1bits.RCIE = 0;
-	RCSTAbits.RX9 = 0;
-	TRISCbits.TRISC6 = 1;
-	TRISCbits.TRISC7 = 1;
-	RCSTAbits.CREN = 1;
+    SPBRG = 77;
+    TXSTAbits.BRGH = 0;
+    BAUDCONbits.BRG16 = 0;
+    TXSTAbits.SYNC = 0;
+    RCSTAbits.SPEN = 1;
+    PIE1bits.TXIE = 0;
+    TXSTAbits.TX9 = 0;
+    TXSTAbits.TXEN = 1;
+    PIE1bits.RCIE = 0;
+    RCSTAbits.RX9 = 0;
+    TRISCbits.TRISC6 = 1;
+    TRISCbits.TRISC7 = 1;
+    RCSTAbits.CREN = 1;
 }
 
 
 void putchar(char c)
 {
-	while( ! PIR1bits.TXIF );
-	TXREG = c;
+    while (! PIR1bits.TXIF);
+    TXREG = c;
 }
 
 
-int getchar(char* c)
+int getchar(char *c)
 {
     // If error reset CREN
-	if (RCSTAbits.OERR)
-	{
-		RCSTAbits.CREN=0;
-		RCSTAbits.CREN=1;
-	}
+    if (RCSTAbits.OERR)
+    {
+        RCSTAbits.CREN = 0;
+        RCSTAbits.CREN = 1;
+    }
 
-	while( ! PIR1bits.RCIF ){ }
+    while (! PIR1bits.RCIF) { }
 
-	*c = RCREG;
-	return 1;
+    *c = RCREG;
+    return 1;
 }
 
 
 void puts(const char *s)
 {
     char c = *s;
-    while( c != '\0' )
+    while (c != '\0')
     {
         putchar(c);
         c = *(++s);
